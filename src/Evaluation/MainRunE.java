c5a84a23812a3c81356f7218cdf4c2a097e3dc89
AjShirin <shirinajmi@gmail.com>
2022-11-29 16:34:18 +0400

    Modifying the search class by displaying the unque search and the duplicate and updating the documentation

--- src/Evaluation/MainRunE.java
+++ src/Evaluation/MainRunE.java
@@ -51,6 +51,8 @@ public class MainRunE {
 				
 				break;
 				
+
+				
 			}
 
 		} while (true);
--- src/Evaluation/Search.java
+++ src/Evaluation/Search.java
@@ -3,15 +3,21 @@ package Evaluation;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Search {
+	static Set<String> hashSearchUnique = new HashSet<String>();
+	static List<String> listDuplicateSearch = new ArrayList<>();
 
 	public static void searchWord() throws IOException {
 
 		/* with modification */
 		Scanner userNumberSc = new Scanner(System.in);
+
 		System.out.println("Enter the number of words you want to search:");
 		Integer numberOfWords = userNumberSc.nextInt();
 
@@ -24,6 +30,9 @@ public class Search {
 			Scanner userSc = new Scanner(System.in);
 			String name = userSc.nextLine();
 
+			listDuplicateSearch.add(name);
+			hashSearchUnique.add(name);
+
 			List<String> lineFromFiles = Files.readAllLines(Paths.get("dogBreedAPI.txt"));
 			for (String lineVariable : lineFromFiles) {
 				if (lineVariable.contains(name)) {
@@ -32,9 +41,25 @@ public class Search {
 					System.out.println(lineVariable);
 					System.out.println("++++++++++++++++");
 				} else {
+
 					System.out.println(name + "  " + "Word not available :(");
+
 				}
+
 			}
+
 		}
+		
+		System.out.println("The Unique search is " + " " + hashSearchUnique);
+
+		for (String search : listDuplicateSearch) {
+			if (hashSearchUnique.add(search) == false) {
+				System.out.println("The Deplicate search are: " + " " + search);
+				
+
+			}
+
+		}
+
 	}
 }
