cd0e4814158aaba2cde50410c775083ed3ee985f
AjShirin <shirinajmi@gmail.com>
2022-11-29 15:51:44 +0400

    Modifying the search for multiple words and updating the documentation

--- src/Evaluation/Search.java
+++ src/Evaluation/Search.java
@@ -9,19 +9,32 @@ import java.util.Scanner;
 public class Search {
 
 	public static void searchWord() throws IOException {
-	    System.out.println("Please Enter the Word you would like to search for: ");
-	    Scanner userSc = new Scanner(System.in);
-	    String name = userSc.nextLine();
 
-	    List<String> lineFromFiles = Files.readAllLines(Paths.get("dogBreedAPI.txt"));
-	    for (String lineVariable : lineFromFiles) {
-	        if (lineVariable.contains(name)) {
-	        	System.out.println("Word : " + name + "  is Available yaay!!! :)"); 
-	            System.out.println(lineVariable);
-	        }
-	        else {
-	        	System.out.println("Word not available :(");
-	        }
-	    }
+		/* with modification */
+		Scanner userNumberSc = new Scanner(System.in);
+		System.out.println("Enter the number of words you want to search:");
+		Integer numberOfWords = userNumberSc.nextInt();
+
+		System.out.println("++++++++++++++++");
+		System.out.println("the search will be done one by one.");
+		System.out.println("++++++++++++++++");
+
+		for (int i = 0; i < numberOfWords; i++) {
+			System.out.println("Please Enter the Word you would like to search for: ");
+			Scanner userSc = new Scanner(System.in);
+			String name = userSc.nextLine();
+
+			List<String> lineFromFiles = Files.readAllLines(Paths.get("dogBreedAPI.txt"));
+			for (String lineVariable : lineFromFiles) {
+				if (lineVariable.contains(name)) {
+					System.out.println("Word : " + name + "  is Available yaay!!! :)");
+					System.out.println("++++++++++++++++");
+					System.out.println(lineVariable);
+					System.out.println("++++++++++++++++");
+				} else {
+					System.out.println(name + "  " + "Word not available :(");
+				}
+			}
+		}
 	}
 }
